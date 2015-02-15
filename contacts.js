688a058750b441ec79c0b6a91ce1b3f65a3f1177
gdivya <gdivyareddy@gmail.com>
2015-02-15 17:52:23 +1100

    converted accordion to angular

--- contacts.js
+++ contacts.js
@@ -20,7 +20,7 @@ var mail=contactinfo.replace("%data%",contacts.E-mail);
  
 */
 
-var contactsApp = angular.module('myblog',['ngRoute']);
+var contactsApp = angular.module('myblog',['ngRoute', 'ui.bootstrap']);
 contactsApp.config(function($routeProvider){
 	$routeProvider.
 	when('/', {
@@ -29,8 +29,8 @@ contactsApp.config(function($routeProvider){
           }).
 
 when('/resume', {
-templateUrl:'myresume.html',
-controller:'ResumeController',
+templateUrl:'resume.html',
+controller:'AccordionDemoCtrl',
 }).
 
 when('/contactme', {
@@ -54,7 +54,31 @@ contactsApp.controller('ResumeController',function(){
 contactsApp.controller('ContactDetailsController',function(){
 	console.log('contact details');
 });
+contactsApp
+.controller('AccordionDemoCtrl', function ($scope) {
+	  $scope.oneAtATime = true;
 
+	  $scope.groups = [
+	    {
+	      title: 'Dynamic Group Header - 1',
+	      content: 'Dynamic Group Body - 1'
+	    },
+	    {
+	      title: 'Dynamic Group Header - 2',
+	      content: 'Dynamic Group Body - 2'
+	    }
+	  ];
 
+	  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
+	  $scope.addItem = function() {
+	    var newItemNo = $scope.items.length + 1;
+	    $scope.items.push('Item ' + newItemNo);
+	  };
+
+	  $scope.status = {
+	    isFirstOpen: true,
+	    isFirstDisabled: false
+	  };
+	});
 
